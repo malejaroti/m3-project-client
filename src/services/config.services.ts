c13210a81ff1dd0d4dbdf596446569a0c6a002a3
Alejandra Rodriguez Tibana <30965660+malejaroti@users.noreply.github.com>
2025-09-16 10:05:00 +0200

    feat(config): Update API baseURL to use environment variable for server URL

--- src/services/config.services.ts
+++ src/services/config.services.ts
@@ -1,7 +1,8 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: `http://localhost:5005/api`,
+  // baseURL: `http://localhost:5005/api`,
+  baseURL: `${import.meta.env.VITE_SERVER_URL}/api`
 });
 
 api.interceptors.request.use((config) => {
