b549f768bcfb29183eec8c9eafbd08251ac9200f
Alejandra Rodriguez Tibana <30965660+malejaroti@users.noreply.github.com>
2025-10-06 12:48:27 +0200

    refactor(App): Update Navbar import path and clean up layout structure

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,7 @@ import SignIn from './sign-in/SignIn.tsx';
 
 // import BorderBox from './components/BorderBox'
 // import Footer from './components/Footer'
-import Navbar from './components/Navbar';
+import Navbar from './components/layout/Navbar.tsx';
 import OnlyPrivate from './components/OnlyPrivate.tsx';
 import TimelinesPage from './pages/TimelinesPage.tsx';
 import TimelineItemsPage from './pages/TimelineItemsPage.tsx';
@@ -20,47 +20,45 @@ import UserProfilePage from './pages/UserProfilePage.tsx';
 function App() {
   return (
     <>
-      <div className="flex min-h-screen bg-white-500">
-        {/* Navbar */}
-        <div className="flex flex-col flex-1">
-          {/* <Navbar/> */}
-          <Navbar />
+      {/* Navbar */}
+      <div className="flex flex-col flex-1">
+        {/* <Navbar/> */}
+        <Navbar />
 
-          {/* Main container */}
-          <main className="w-full flex-1 bb-red px-10">
-            {/* <div className='central-section bb-black h-full w-[50%] m-auto'> */}
-            <Routes>
-              <Route path="/timelines" 
-                      element={ <OnlyPrivate> {' '}
-                                  <TimelinesPage />{' '}
-                                </OnlyPrivate>
-                }
-              />
-              <Route path="/timeline/:timelineId" element={<OnlyPrivate>{' '}<TimelineItemsPage />{' '} </OnlyPrivate>} />
-              <Route path="/timeline/:timelineId" element={<OnlyPrivate>{' '}<TimelineItemsPage />{' '} </OnlyPrivate>} />
-              <Route path="/lifetimeline" element={<OnlyPrivate> <LifeTimeline /> </OnlyPrivate>} />
-              <Route path="/user-profile" element={<OnlyPrivate> <UserProfilePage /> </OnlyPrivate>} />
-              <Route path="/" element={<Home />} />
-              <Route path="/sign-up" element={<SignUp />} />
-              <Route path="/sign-in" element={<SignIn />} />
+        {/* Main container */}
+        <main className="max-w-full flex-1 px-10 py-5 ">
+          {/* <div className='central-section bb-black h-full w-[50%] m-auto'> */}
+          <Routes>
+            <Route path="/timelines"
+              element={<OnlyPrivate> {' '}
+                <TimelinesPage />{' '}
+              </OnlyPrivate>
+              }
+            />
+            <Route path="/timeline/:timelineId" element={<OnlyPrivate>{' '}<TimelineItemsPage />{' '} </OnlyPrivate>} />
+            <Route path="/timeline/:timelineId" element={<OnlyPrivate>{' '}<TimelineItemsPage />{' '} </OnlyPrivate>} />
+            <Route path="/lifetimeline" element={<OnlyPrivate> <LifeTimeline /> </OnlyPrivate>} />
+            <Route path="/user-profile" element={<OnlyPrivate> <UserProfilePage /> </OnlyPrivate>} />
+            <Route path="/" element={<Home />} />
+            <Route path="/sign-up" element={<SignUp />} />
+            <Route path="/sign-in" element={<SignIn disableCustomTheme={false} />} />
 
-              <Route path="/error" element={<ErrorPage />} />
-              <Route path="*" element={<NotFoundPage />} />
-              {/* <Route path="/about" element={<AboutPage />} /> */}
+            <Route path="/error" element={<ErrorPage />} />
+            <Route path="*" element={<NotFoundPage />} />
+            {/* <Route path="/about" element={<AboutPage />} /> */}
 
-            </Routes>
-            {/* </div> */}
-          </main>
-          {/* <BorderBox borderColor={"red"}>
+          </Routes>
+          {/* </div> */}
+        </main>
+        {/* <BorderBox borderColor={"red"}>
             <main className='w-full flex-1 bb'>
             </main>
           </BorderBox> */}
-          {/* <Footer/> */}
-          {/* <footer className="w-full h-[60px] bb-black"></footer> */}
-        </div>
-        {/* Main page */}
-        {/* Footer */}
+        {/* <Footer/> */}
+        {/* <footer className="w-full h-[60px] bb-black"></footer> */}
       </div>
+      {/* Main page */}
+      {/* Footer */}
     </>
   );
 }
