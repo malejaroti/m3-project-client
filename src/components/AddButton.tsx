e0558b369a39d495ceac0dd29c3811b457274be9
Alejandra Rodriguez Tibana <30965660+malejaroti@users.noreply.github.com>
2025-10-06 12:10:15 +0200

    refactor(AddButton): remove absolute positioning styles for better layout flexibility

--- src/components/AddButton.tsx
+++ src/components/AddButton.tsx
@@ -14,9 +14,6 @@ function AddButton({ onClick, buttonLabel}: AddButtonProps) {
                 fontSize: { xs: "0.7rem", sm: "0.85rem", md: "1rem" },
                 px: { xs: 1, sm: 2, md: 3 },
                 py: { xs: 0.5, sm: 1 },
-                position: 'absolute',
-                top: 0, 
-                right: 0,
                 bgcolor: 'primary.main', // use theme's color
                 '&:hover': {
                     bgcolor: 'primary.dark',
